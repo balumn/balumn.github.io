b99b089fb7b76cf179d6fda4b6fed827152c16e9
balumn <balumn@ieee.org>
2019-07-28 19:54:00 +0530

    minor updates

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
 import Portfolio from './components/Portfolio';
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div >
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Portfolio} />
-          </Switch>
-        </Router>
+        <Portfolio />
       </div>
     );
   }
